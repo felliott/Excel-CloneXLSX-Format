e93ab2e9e41eeed0e6d8824a4fc7c0b3a2c00d4e
Fitz Elliott <felliott@fiskur.org>
2014-06-27 16:50:55 -0400

    initial version of working module (w/ tests)

--- lib/Excel/CloneXLSX/Format.pm
+++ lib/Excel/CloneXLSX/Format.pm
@@ -1,9 +1,62 @@
 package Excel::CloneXLSX::Format;
+
 use 5.008001;
 use strict;
 use warnings;
 
 our $VERSION = "0.01";
+
+use Exporter 'import';
+our @EXPORT_OK = qw(translate_xlsx_format);
+
+sub translate_xlsx_format {
+    my ($old_fmt) = @_;
+    return {} unless (defined $old_fmt);
+
+    # font
+    my $old_font = $old_fmt->{Font};
+    my %new_fmt  = (
+        font           => $old_font->{Name},
+        size           => $old_font->{Height},
+        color          => $old_font->{Color},
+        bold           => $old_font->{Bold},
+        italic         => $old_font->{Italic},
+        underline      => $old_font->{UnderlineStyle},
+        font_strikeout => $old_font->{Strikeout},
+        font_script    => $old_font->{Super},
+    );
+
+    # shading
+    my $old_shading = $old_fmt->{Fill};
+    my @shading_fields = qw(pattern fg_color bg_color);
+    for my $idx (0..$#shading_fields) {
+        $new_fmt{ $shading_fields[$idx] } = $old_shading->[$idx]
+            if (defined $old_shading->[$idx]);
+    }
+
+    # if pattern is 'solid', Excel swaps fgColor and bgColor. This unswaps.
+    if ($new_fmt{pattern} == 1) {
+        @new_fmt{qw(bg_color fg_color)} = @new_fmt{qw(fg_color bg_color)};
+    }
+
+    # alignment
+    # halign numbers match up, valign numbers are off by one
+    my ($old_halign, $old_valign) = @{$old_fmt}{qw(AlignH AlignV)};
+    $new_fmt{text_h_align} = $old_halign;
+    $new_fmt{text_v_align} = defined($old_valign) ? $old_valign+1 : 0;
+
+    # borders
+    my $old_border_style = $old_fmt->{BdrStyle};
+    my $old_border_color = $old_fmt->{BdrColor};
+    my @sides = qw(left right top bottom);
+    for my $idx (0..$#sides) {
+        my $side = $sides[$idx];
+        $new_fmt{$side} = $old_border_style->[$idx] if ($old_border_style->[$idx]);
+        $new_fmt{"${side}_color"} = $old_border_color->[$idx] if ($old_border_color->[$idx]);
+    }
+
+    return \%new_fmt;
+}
 
 
 
@@ -14,15 +67,63 @@ __END__
 
 =head1 NAME
 
-Excel::CloneXLSX::Format - It's new $module
+Excel::CloneXLSX::Format - Convert Spreadsheet::ParseXLS formats to Excel::Writer::XLSX
 
 =head1 SYNOPSIS
 
-    use Excel::CloneXLSX::Format;
+    use Excel::CloneXLSX::Format qw(translate_xlsx_format);
+
+    my $old_workbook = Spreadsheet::ParseXLSX->new->parse('data/sample.xlsx');
+    my $old_worksheet = $old_workbook->worksheet('Sheet1');
+    my $old_cell = $old_worksheet->get_cell(0,0);
+    my $old_format = $old_cell->get_format();
+
+    my $new_workbook = Excel::Writer::XLSX->new;
+    my $new_worksheet = $new_workbook->add_worksheet();
+
+    my $fmt_props  = translate_xlsx_format( $old_format );
+    my $new_format = $new_workbook->add_format(%$fmt_props);
+    $new_worksheet->write(0, 0, $old_cell->value, $new_format);
+
 
 =head1 DESCRIPTION
 
-Excel::CloneXLSX::Format is ...
+CPAN has a create module for reading XLSX files
+(L<Spreadsheet::ParseExcel>), and a great module for writing XLSX
+files (L<Excel::Writer::XLSX>), but no module for editing XLSX
+files. B<< L<Excel::CloneXLSX::Format> >>... won't do that either.  It
+will convert L<Spreadsheet::ParseExcel>-style cell formats to a
+structure that L<Excel::Writer::XLSX> will understand.
+
+=head1 USAGE
+
+=head2 translate_xlsx_format( $cell->get_format() )
+
+Takes the hashref returned from Spreadsheet::ParseExcel's get_format
+method and returns a hashref that can be fed to Excel::Writer::XLSX's
+new_format method.
+
+=head3 What's Supported
+
+=over
+
+=item * Font (Family, Style, Size, {Super,Sub}script
+
+=item * Background Color
+
+=item * Alignment
+
+=item * Border Style and Color
+
+=back
+
+=head3 What isn't
+
+=over
+
+=item * Everything else
+
+=back
 
 =head1 LICENSE
 
