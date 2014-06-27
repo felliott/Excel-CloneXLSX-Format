a91d2ecd1897e40bce09d41cab80efdd3164d2bb
Fitz Elliott <felliott@fiskur.org>
2014-06-27 14:50:20 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Excel::CloneXLSX::Format - It's new $module
+
+# SYNOPSIS
+
+    use Excel::CloneXLSX::Format;
+
+# DESCRIPTION
+
+Excel::CloneXLSX::Format is ...
+
+# LICENSE
+
+Copyright (C) Fitz Elliott.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Fitz Elliott <felliott@fiskur.org>
---
+++ lib/Excel/CloneXLSX/Format.pm
@@ -0,0 +1,39 @@
+package Excel::CloneXLSX::Format;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Excel::CloneXLSX::Format - It's new $module
+
+=head1 SYNOPSIS
+
+    use Excel::CloneXLSX::Format;
+
+=head1 DESCRIPTION
+
+Excel::CloneXLSX::Format is ...
+
+=head1 LICENSE
+
+Copyright (C) Fitz Elliott.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Fitz Elliott E<lt>felliott@fiskur.orgE<gt>
+
+=cut
+
