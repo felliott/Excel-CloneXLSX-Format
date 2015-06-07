93d4f6f020929c80f3aa208fbbde5c0f6f861df1
Fitz Elliott <felliott@fiskur.org>
2015-06-07 10:15:21 -0600

    doc: more minor pod fixes

--- lib/Excel/CloneXLSX/Format.pm
+++ lib/Excel/CloneXLSX/Format.pm
@@ -67,40 +67,40 @@ __END__
 
 =head1 NAME
 
-Excel::CloneXLSX::Format - Convert Spreadsheet::ParseXLS formats to Excel::Writer::XLSX
+Excel::CloneXLSX::Format - Convert Spreadsheet::ParseXLSX formats to Excel::Writer::XLSX
 
 =head1 SYNOPSIS
 
-    use Excel::CloneXLSX::Format qw(translate_xlsx_format);
-    use Excel::Writer::XLSX;
-    use Safe::Isa;
-    use Spreadsheet::ParseXLSX;
+ use Excel::CloneXLSX::Format qw(translate_xlsx_format);
+ use Excel::Writer::XLSX;
+ use Safe::Isa;
+ use Spreadsheet::ParseXLSX;
 
-    my $old_workbook  = Spreadsheet::ParseXLSX->new->parse('t/data/sample.xlsx');
-    my $old_worksheet = $old_workbook->worksheet('Sheet1');
+ my $old_workbook  = Spreadsheet::ParseXLSX->new->parse('t/data/sample.xlsx');
+ my $old_worksheet = $old_workbook->worksheet('Sheet1');
 
-    open my $fh, '>', 't/data/converted.xlsx'
-        or die "Can't open output: $!";
-    my $new_workbook  = Excel::Writer::XLSX->new( $fh );
-    my $new_worksheet = $new_workbook->add_worksheet();
+ open my $fh, '>', 't/data/converted.xlsx'
+     or die "Can't open output: $!";
+ my $new_workbook  = Excel::Writer::XLSX->new( $fh );
+ my $new_worksheet = $new_workbook->add_worksheet();
 
-    my ($row_min, $row_max) = $old_worksheet->row_range();
-    my ($col_min, $col_max) = $old_worksheet->col_range();
-    for my $row ($row_min..$row_max) {
-        for my $col ($col_min..$col_max) {
+ my ($row_min, $row_max) = $old_worksheet->row_range();
+ my ($col_min, $col_max) = $old_worksheet->col_range();
+ for my $row ($row_min..$row_max) {
+     for my $col ($col_min..$col_max) {
 
-            my $old_cell   = $old_worksheet->get_cell($row, $col);
-            my $old_format = $old_cell->$_call_if_object('get_format');
-            my $fmt_props  = translate_xlsx_format( $old_format );
-            my $new_format = $new_workbook->add_format(%$fmt_props);
-            $new_worksheet->write(
-                $row, $col, ($old_cell->$_call_if_object('unformatted') || ''),
-                $new_format
-            );
-        }
-    }
+         my $old_cell   = $old_worksheet->get_cell($row, $col);
+         my $old_format = $old_cell->$_call_if_object('get_format');
+         my $fmt_props  = translate_xlsx_format( $old_format );
+         my $new_format = $new_workbook->add_format(%$fmt_props);
+         $new_worksheet->write(
+             $row, $col, ($old_cell->$_call_if_object('unformatted') || ''),
+             $new_format
+         );
+     }
+ }
 
-    $new_workbook->close;
+ $new_workbook->close;
 
 
 =head1 DESCRIPTION
